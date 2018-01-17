6d06ec4b99908d49f771cd1c3a733584d00f74f5
ahuerta <ahuerta@adglow.com>
2018-01-17 17:20:31 +0100

    basic example completed, you can win and you can lose

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
 var redGamePiece, blueGamePiece, yellowGamePiece;
 var myScore;
+var upBoundary ,downBoundary,leftBoundary, rightBoundary;
 //Game  manager
 function startGame() {
     myGameArea.start();
-    redGamePiece = new component(40, 40, "red", 10, 126);
-    blueGamePiece = new component(40, 40, "blue", 400, 126);
-    yellowGamePiece = new component(75, 470, "yellow", 188, 0);
+    redGamePiece = new component(40, 40, "red", 10, 240);
+    blueGamePiece = new component(40, 40, "blue", 430, 240);
+    upBoundary = new component(478, 10, "green", 0, 0);
+    downBoundary = new component(478, 10, "green", 0, 470);
+    leftBoundary = new component(10, 478, "green", 470, 0);
+    rightBoundary = new component(10, 478, "green", 0, 0);
+    yellowGamePiece = new component(75, 460, "yellow", 190, 10);
     myScore = new component("30px", "Consolas", "black", 280, 40, "text");
     myGameArea.start();
 }
@@ -14,7 +19,7 @@ var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
         this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.height = 480;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
@@ -23,7 +28,12 @@ var myGameArea = {
     },
     clear : function() {
      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
+        this.clear();
     }
+
 };
 // Player Definition
 function component(width, height, color, x, y, type) {
@@ -49,19 +59,49 @@ function component(width, height, color, x, y, type) {
         this.x += this.speedX;
         this.y += this.speedY;
     };
+  this.crashWith = function(otherobj) {
+      var myleft = this.x;
+      var myright = this.x + (this.width);
+      var mytop = this.y;
+      var mybottom = this.y + (this.height);
+      var otherleft = otherobj.x;
+      var otherright = otherobj.x + (otherobj.width);
+      var othertop = otherobj.y;
+      var otherbottom = otherobj.y + (otherobj.height);
+      var crash = true;
+      if ((mybottom < othertop) ||
+             (mytop > otherbottom) ||
+             (myright < otherleft) ||
+             (myleft > otherright)) {
+         crash = false;
+      }
+      return crash;
+  };
 }
 
 // Frame update
 function updateGameArea() {
-    myGameArea.clear();// If commented will leave a trail like tron
-    myScore.text="SCORE: " + myGameArea.frameNo;
-    if(this.myGameArea.start.started){ myGameArea.frameNo += 1 ;}
-    myScore.update();
-    redGamePiece.newPos();
-    blueGamePiece.newPos();
-    redGamePiece.update();
-    blueGamePiece.update();
-    yellowGamePiece.update();// the order does matter
+  if (redGamePiece.crashWith(blueGamePiece)) {
+          alert("BUUUM");
+          restart();
+      } else {
+      myGameArea.clear();// If commented will leave a trail like tron
+      myScore.text="SCORE: " + myGameArea.frameNo;
+      if(this.myGameArea.start.started){ myGameArea.frameNo += 1 ;}
+      myScore.update();
+      // Boundaries
+      upBoundary.update();
+      downBoundary.update();
+      leftBoundary.update();
+      rightBoundary.update();
+      // Planes
+      redGamePiece.newPos();
+      blueGamePiece.newPos();
+      redGamePiece.update();
+      blueGamePiece.update();
+      // blankZone
+      yellowGamePiece.update();// the order does matter
+    }
 }
 
 function start() {
@@ -69,9 +109,16 @@ function start() {
   redGamePiece.speedX += 1;
   blueGamePiece.speedX -= 1;
 }
-function stop() {
+function restart() {
   this.myGameArea.start.started = false;
   redGamePiece.speedX -= 1;
   blueGamePiece.speedX += 1;
   myGameArea.frameNo -= 1;
+    myGameArea.stop();
+    startGame();
+}
+function stop() {
+  alert("Buff you saved a Crash, maybe next time you can do it better?");
+  restart();
+
 }
